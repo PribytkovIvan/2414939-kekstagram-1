dd41705d90b626bc484054e6e6cf20f7b08bbdf0
Иван <orient@211.ru>
2025-05-15 13:53:30 +0700

    правки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { generatePhotosData } from './data.js';
 
+
 const photosData = generatePhotosData();
 console.log(photosData);
 
 import { renderThumbnails } from './thumbnails.js';
-
 renderThumbnails();
+
